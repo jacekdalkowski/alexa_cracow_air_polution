2a45bf6365be3d069902b2b9a74ba3baee78f7f0
Jacek Dalkowski <jacekdalkowski@gmail.com>
2017-06-12 23:37:01 +0200

    Adds request verifier.

--- api/alexa_cracow_air_polution/app.js
+++ api/alexa_cracow_air_polution/app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var log4js = require("log4js");
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
@@ -17,14 +18,23 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+var log4jsLogger = log4js.getLogger();
+var morganLogger = morgan({
+  "format": "default",
+  "stream": {
+    write: function(str) { log4jsLogger.debug(str); }
+  }
+});
+app.use(morganLogger);
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- api/alexa_cracow_air_polution/routes/api.js
+++ api/alexa_cracow_air_polution/routes/api.js
@@ -1,18 +1,57 @@
 var express = require('express');
+var alexaVerifier = require('alexa-verifier-middleware');
 var router = express.Router();
+
+router.use(alexaVerifier);
 
 router.get('/', function(req, res, next) {
   res.send({ 'test': 'value' });
 });
 
-router.get('/airpolution', function(req, res, next) {
+router.post('/airpolution', function(req, res, next) {
   var collection = req.app.locals.db.collection('airpolution');
 
   collection
   .find()
   .toArray(function(err, items) {
-    res.json(items[0]);
+    res.json(buildAlexaResponse(items[0]));
   });
 });
+
+function mapPolutionDataToQualityIndex(pollution_data){
+  var pm10Level = pollution_data['pm_10']
+  if(pm10Level < 25){
+    return "good";
+  }else if(pm10Level < 50){
+    return "moderate";
+  }else if(pm10Level < 75){
+    return "unhealthy";
+  }else if(pm10Level < 100){
+    return "very unhealthy";
+  }else{
+    return "hazardous";
+  }
+}
+
+function buildAlexaResponse(pollution_data){
+
+  var pm10Level = pollution_data['pm_10'];
+  var qualityIndex = mapPolutionDataToQualityIndex(pollution_data);
+
+  var r = { 
+  "version": "0.0.1", 
+  "response": { 
+    "outputSpeech": { 
+      "type": "SSML", 
+      "ssml": "<speak>" +
+                "Air is <prosody volume='x-loud' pitch='x-low'>" + qualityIndex + "</prosody>. PM 10 level is " + pollution_data['pm_10'] + "." +
+              "</speak>", 
+    }
+  },
+  "shouldEndSession": true 
+  };
+
+  return r;
+}
 
 module.exports = router
